c71248ad05d95072aa7677df71be998c586e47d6
Bing Du <Bing.Du@akqa.com>
2014-03-03 10:00:31 +0800

    update

---
+++ CornettosLottery/PropertyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CornettosLottery
+{
+    public static class PropertyHelper
+    {
+        public static string PropertyFormatValue(this object xProperty,int number)
+        {
+            return xProperty.ToString().PadRight(number, ' ');
+        }
+    }
+}
